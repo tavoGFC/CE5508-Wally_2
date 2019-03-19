dbf5b38fed1ac7463c56d80a21ea75fa9ef135f8
tavoGFC <gusfcarrera@hotmail.es>
2019-03-19 00:27:31 -0600

    Update deslizador_motor.ino

--- deslizador_motor/deslizador_motor.ino
+++ deslizador_motor/deslizador_motor.ino
@@ -1,26 +1,13 @@
-/*
-  www.diymakers.es
-  by A.García
-  Control velocidad y sentido motor DC
-  Tutorial en: http://diymakers.es/control-velocidad-y-sentido-de-motor-dc/
-*/
+//----------------------
  
-int pin2=9;   //Entrada 2 del L293D
-int pin7=10;  //Entrada 7 del L293D
-int valorpote;  
-int pwm1;          //Variable del PWM 1
-int pwm2;          //Variable del PWM 2
+int pinA=9;   //Entrada 2 del L293D
+int pinB=10;  //Entrada 7 del L293D
+int valueDirectionCompressor; //indica la direccion del compresor //valores 0 y 1023
+bool isActivatedCompressor;
 
-bool estadoLed = false;  //guarda el estado del led (encendido o apagado)
-int periodo = 5000;  // tiempo que esta el LED en alto y bajo
+//------------------------------------
+int periodo = 5000;  // tiempo que el motor dura en hacer media vuelta (bajar por completo o subir por completo)
 unsigned long tiempoAnterior = 0;  //guarda tiempo de referencia para comparar
-
-
-const int analogInPin = A1; // Pin analogico 1 para la lectura del Fototransistor
-const int OutPin = 13;      // Pin para el led indicador
-
-int sensorValue = 0;        // Inicializamos el valor del sensor
-
 
 void setup()
 {
@@ -29,58 +16,43 @@ void setup()
   pinMode(pin2,OUTPUT);
   pinMode(pin7, OUTPUT);
 }
- 
-void loop()
-{
-   LedIR();
-   //Motor();
-}
 
-void Motor(){
+void motor(){
+  //se activa el compresor (empieza a bajar)
+  isActivatedCompressor = true;
+  bool bajando_compresor = true;
+
+  //se le da el valor para bajar el compresor
+  if (bajando_compresor){
+    //se inicia el motor con la direccion 0 (P.D: probar que direccion es el valor cero)
+    directionCompressor = 0;
+  } else{
+    //se le da la direccion para que el compresor suba 
+    directionCompressor = 1023;
+  }
+
+  //Como esperar el tiempo de que dura en bajar y subir para dejar de llamar la funcion motor()???
+
+  
+  //se espera 5000 ms la variable directionCompressor en 0
+  //transcurrido el tiempo necesario para llegar abajo se cambia la booleana bajando -> subiendo
+  if(millis()-tiempoAnterior >= periodo){  //si ha transcurrido el periodo programado
+    bajando_compresor = false;
+    tiempoAnterior=millis();  //guarda el tiempo actual como referencia
+  }
+
+  /*Se le manda el valor al motor*/
+  pwm1 = map(directionCompressor, 0, 1023, 0, 255);
+  pwm2 = map(directionCompressor, 0, 1023, 255, 0);
+  analogWrite(pinA, pwm1);
+  analogWrite(pinB, pwm2);
+  
+  /*
+  //al pasar 5000ms(tiempo estimado en lo que dura en hacer media vuelta, sea bajar o subir) se cambia la direccion del motor con el valor 1023
   if(millis() - tiempoAnterior >= periodo){  //si ha transcurrido el periodo programado
-    estadoLed =! estadoLed;  //cambia el estado del led cada 100ms
+    directionCompressor = 1023;  //cambia el estado del led cada 100ms
     tiempoAnterior = millis();  //guarda el tiempo actual como referencia
    }
-   
-   valorpote=0;
-   if (estadoLed){
-    valorpote=1023;
-   }
+   */
 
-   /*
-   Serial.print("Direccion: " );
-   Serial.println(valorpote);
-  */
-   
-  //Como la entrada analógica del Arduino es de 10 bits, el rango va de 0 a 1023.
-  //En cambio, la salidas del Arduio son de 8 bits, quiere decir, rango entre 0 a 255.
-  //Por esta razón tenemos que mapear el número de un rango a otro usando este código.
-  pwm1 = map(valorpote, 0, 1023, 0, 255);
-  pwm2 = map(valorpote, 0, 1023, 255, 0); //El PWM 2 esta invertido respecto al PWM 1
- 
-  //Sacamos el PWM de las dos salidas usando analogWrite(pin,valor)
-  analogWrite(pin2,pwm1);
-  analogWrite(pin7,pwm2);
-}
-
-void LedIR(){
-  // leemos el pin para y asignamos el valor a la variable.
-  sensorValue = analogRead(analogInPin);            
-  
-  // Si el valor obtenido es mayor a 900 se activa el LED
-  if(sensorValue > 900)
-  {
-    Serial.println("Deteccion > 900");
-    Motor();
-  }
-  else
-  {
-    Serial.println("Deteccion < 900");
-  }
-
-  // Imprimimos el valor en el monitor.
-  Serial.print("sensor = " );                       
-  Serial.println(sensorValue);     
-
-  delay(100);                 
 }
