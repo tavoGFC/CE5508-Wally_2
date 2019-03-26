33f882f29355b364bf4ab72f3d6e24014f804c8f
tavoGFC <gusfcarrera@hotmail.es>
2019-03-25 21:40:33 -0600

    delete node_modules

---
+++ code_app/Wally/Screens/Home-Page/index.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { 
+  Text, 
+  View, 
+  StyleSheet,
+  TextInput, 
+  Button, 
+  Image, 
+  ActivityIndicator
+  } from 'react-native';
+
+
+  
+export default class HomaPage extends React.Component {
+    
+    render() {
+        return (
+            <View style={styles.container}>
+              <Text>Open up App.js to start working on your app!</Text>
+            </View>
+          );
+    }
+
+}
