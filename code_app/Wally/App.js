d5d064cca80d04836a1a273f1334bd7c9f799049
tavoGFC <gusfcarrera@hotmail.es>
2019-03-26 02:54:02 -0600

    add GridView/HomePage

--- code_app/Wally/App.js
+++ code_app/Wally/App.js
@@ -1,21 +1,22 @@
-import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { createStackNavigator } from "react-navigation";
+import * as React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createStackNavigator, createAppContainer } from "react-navigation";
+//import LogIn from "./screens/log-in";
+import Home from "./screens/home-page";
 
-const App = createStackNavigator({
-  Login: {screen: LoginPage},
-  Home: {screen: SearchPage},
-  Results: {screen: SearchResults}
-})
+const AppStack = createStackNavigator({
+  Home: { screen: Home }
+});
 
+const App = createAppContainer(AppStack);
 
-export default App
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
--- code_app/Wally/Screens/Home-Page/index.js
+++ code_app/Wally/Screens/Home-Page/index.js
@@ -1,24 +1,77 @@
 import * as React from 'react';
-import { 
-  Text, 
-  View, 
-  StyleSheet,
-  TextInput, 
-  Button, 
+import {
+  Alert,
+  ActivityIndicator,
+  Button,
+  FlatList, 
   Image, 
-  ActivityIndicator
+  StyleSheet,
+  Text, 
+  TextInput, 
+  View 
   } from 'react-native';
 
+export default class HomePage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      dataSource: [
+        {key: 'One'},
+        {key: 'Two'}
+      ]
+    };
+  }
 
-  
-export default class HomaPage extends React.Component {
-    
-    render() {
-        return (
-            <View style={styles.container}>
-              <Text>Open up App.js to start working on your app!</Text>
+  _getGridViewItem (item) {
+    Alert.alert(item);
+  }
+
+  render() {
+    return (
+      <View style={styles.MainContainer}>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({ item }) => (
+            <View style={styles.gridViewColumns}>
+              <Text style={styles.gridViewItem} 
+              onPress={this._getGridViewItem.bind(this, item.key)}> 
+              {item.key} 
+              </Text>
             </View>
-          );
-    }
-
+          )}
+          //Setting the number of column
+          numColumns={2}
+        />
+      </View>
+    );
+  }
 }
+ 
+const styles = StyleSheet.create({
+  MainContainer: {
+    justifyContent: 'center',
+    flex: 1,
+    paddingTop: 30,
+  },
+  imageThumbnail: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 100,
+  },
+  gridViewColumns: {
+    justifyContent: 'center',
+    flex:1,
+    alignItems: 'center',
+    height: 100,
+    margin: 5,
+    backgroundColor: '#00BCD4'
+  },
+  gridViewItem: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 100,
+    color: '#34fe65',
+    padding: 10,
+    fontSize: 10,
+  }
+});
