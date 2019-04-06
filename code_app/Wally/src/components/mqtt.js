3883d8952995d6e016fdaa31cce9493a27fd7b40
Randy <randyma01@gmail.com>
2019-04-06 17:14:45 -0600

    Recuperando MQTT.js

---
+++ code_app/Wally/src/components/mqtt.js
@@ -0,0 +1,59 @@
+import init from 'react_native_mqtt';
+import { AsyncStorage } from 'react-native';
+
+export default function MQTTClient() {
+  init({
+    size: 10000,
+    storageBackend: AsyncStorage,
+    defaultExpires: 1000 * 3600 * 24,
+    enableCache: true,
+    reconnect: true,
+    sync: {}
+  });
+
+  function onConnect() {
+    console.log('onConnect');
+
+    const topic = 'Wally/controll';
+    client.subscribe(topic);
+    message = new Paho.MQTT.Message('abrir,0,');
+    message.destinationName = topic;
+    client.send(message);
+  }
+
+  function onConnectionLost(responseObject) {
+    if (responseObject.errorCode !== 0) {
+      console.log('onConnectionLost:' + responseObject.errorMessage);
+    }
+  }
+
+  function onMessageArrived(message) {
+    console.log('onMessageArrived:' + message.payloadString);
+  }
+
+  function doFail(e) {
+    console.log('error', e);
+  }
+
+  const client = new Paho.MQTT.Client(
+    'm16.cloudmqtt.com',
+    //32692,
+    32757,
+    'web_' + parseInt(Math.random() * 100, 10)
+  );
+
+  client.onConnectionLost = onConnectionLost;
+  client.onMessageArrived = onMessageArrived;
+
+  const options = {
+    useSSL: true,
+    userName: 'auggeqol',
+    password: 'r5ZWFcewuPZS',
+    onSuccess: onConnect,
+    onFailure: doFail
+  };
+
+  client.connect(options);
+
+  return client;
+}
