8072a073078df07cbbb23b2fe216fdd944c13bc7
Randy <randyma01@gmail.com>
2019-04-12 21:25:37 -0600

    Haciendo la Configuracion

--- code_app/Wally/src/screens/settings-page/index.js
+++ code_app/Wally/src/screens/settings-page/index.js
@@ -2,14 +2,35 @@ import * as React from 'react';
 import { Text } from 'react-native';
 
 import stylesSettings from '../../styles/styles';
+import MQTTClient from '../../../components/mqtt';
 
 export default class SettingsPage extends React.Component {
   static navigationOptions = {
     title: 'Configuración'
   };
 
+  constructor(props) {
+    super(props);
+  }
+
   render() {
-    return <Text style={styles.description}>CONFIGURACIONES</Text>;
+    return (
+      <View style={styles.containerTabController}>
+        <Text style={styles.titleTabDashboard}>Ajustes de Wally:</Text>
+        <View style={styles.containerTabController}>
+          <Text style={styles.descriptionWarningController}>
+            Ajuste el peso tara de las pesas de Wally.
+          </Text>
+          <TouchableOpacity
+            onPress={() => {
+              MQTTClient('tare');
+            }}
+          >
+            <Text style={styles.button}>Ajustar Peso Tara</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
   }
 }
 
